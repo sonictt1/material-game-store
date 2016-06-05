bdb4e726e5b7d7ee5dc97fd58047a142b1c58906
root <root@DESKTOP-66QHM0F.localdomain>
2016-06-05 05:36:03 +0000

    added js to game list page

--- static/js/register_sw.js
+++ static/js/register_sw.js
@@ -2,7 +2,19 @@ if ('serviceWorker' in navigator) {
  console.log('Service Worker is supported');
  navigator.serviceWorker.register('./push_notifications.js').then(function(reg) {
    console.log(':^)', reg);
-   // TODO
+   if ('serviceWorker' in navigator) {
+    console.log('Service Worker is supported');
+    navigator.serviceWorker.register('./push_notifications.js').then(function(reg) {
+        console.log(':^)', reg);
+        reg.pushManager.subscribe({
+            userVisibleOnly: true
+        }).then(function(sub) {
+            console.log('endpoint:', sub.endpoint);
+        });
+    }).catch(function(error) {
+        console.log(':^(', error);
+    });
+}
  }).catch(function(err) {
    console.log(':^(', err);
  });
